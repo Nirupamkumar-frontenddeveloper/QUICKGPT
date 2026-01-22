a93b4ecd5d228bb501386415b21ec2c4325cd7d8
Nirupamkumar-frontenddeveloper <nirupamkumar2040@gmail.com>
2026-01-22 15:00:23 +0530

    feat: implement sidebar menu functionality and integrate assets

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,13 +4,27 @@ import Credits from "./pages/Credits";
 import Chatbot from "./components/Chatbot";
 import Community from "./pages/Community";
 import Sidebar from "./components/Sidebar";
+import { assets } from "./assets/assets";
 
 const App = () => {
+
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
   return (
     <>
+{!isMenuOpen && (
+  <img
+    src={assets.menu_icon}
+    onClick={() => setIsMenuOpen(true)}
+    className="absolute top-3 left-3 w-8 h-8 cursor-pointer md:hidden invert dark:invert-0"
+    alt="menu"
+  />
+)}
+
+
+
       <div className='dark:bg-gradient-to-b from-[#241240] to-[#000000] dark:text-white'>
         <div className="flex h-screen w-screen">
-          <Sidebar/>
+          <Sidebar isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
           <Routes>
             <Route path="/credits" element={<Credits />} />
             <Route path="/" element={<Chatbot />} />
--- client/src/components/Chatbot.jsx
+++ client/src/components/Chatbot.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Chatbot = () => {
   return (
     <div>
-      Chatbot Component
+     
     </div>
   )
 }
